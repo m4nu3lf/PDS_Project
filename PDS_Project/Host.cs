6a4b487687525739485c3efa5f2f494dbb9ada94
nemo-91 <adrn.agst@gmail.com>
2014-05-22 13:50:22 +0200

    "prova caricamento"

---
+++ PDS_Project/Host.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace PDS_Project
+{
+    public class Host
+    {
+        private Socket eventSocket;
+        private Socket clipboardSocket;
+
+        public Host(ref Socket es, ref Socket cs )
+        {
+            eventSocket = es;
+            clipboardSocket = cs;
+        }
+
+        public void SendMsg(Message m)
+        {
+
+        }
+
+        public void SendClipboard(Message m)
+        {
+
+        }
+
+        public String ReceiveClipboard()
+        {
+            return "prova";
+        }
+
+    }
+
+}
---
+++ PDS_Project/Message.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDS_Project
+{
+    enum CmType : byte
+    {
+        Authentication,
+        EnterComm,
+        ExitComm
+    }
+
+    public class Message
+    {
+        private CmType cmType; 
+    
+
+        protected Message(CmType cmt){
+            cmType = cmt;
+        }
+
+        public CmType type
+        {
+            get { return cmType; }
+        }
+
+    }
+
+    public class AuthMsg : Message
+    {
+        String psw;
+
+        public AuthMsg(CmType cmt, String p) 
+            : base(cmt) 
+        {
+            psw = p;
+        }
+    
+    }
+
+
+    public class KeyMsg : Message
+    {
+        public KeyMsg(CmType cmt)
+            : base(cmt)
+        {
+
+        }
+    }
+
+
+    public class MouseMsg : Message
+    {
+        public MouseMsg(CmType cmt)
+            : base(cmt)
+        {
+
+        }
+    }
+
+
+    public class DataMsg : Message
+    {
+        private String clipboard;
+
+        public DataMsg(CmType cmt, String clb)
+            : base(cmt)
+        {
+            clipboard = clb;
+        }
+
+        public String clb
+        {
+            get { return clipboard; }
+        }
+    }
+
+}
