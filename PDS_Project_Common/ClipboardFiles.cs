d3122142d8b7f6938f6be44095ddf1032f59052f
m4nu3lf <m4nu3lf@gmail.com>
2014-10-10 18:51:03 +0200

    Adds ClipboardFiles

---
+++ PDS_Project_Common/ClipboardFiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace PDS_Project_Common
+{
+    class ClipboardFiles
+    {
+        static void SendClipboardFiles(Socket socket)
+        {
+
+        }
+
+        static void RecvClipboardFiles(Socket socket)
+        {
+
+        }
+    }
+}
